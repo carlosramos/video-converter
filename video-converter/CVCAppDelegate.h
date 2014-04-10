53dbf0c7f14777bae17edef4f31e2a36c8feea64
Carlos Ramos <carlos@carlosramos.me>
2014-04-10 12:48:43 +0200

    Initial Commit

---
+++ video-converter/CVCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CVCAppDelegate.h
+//  video-converter
+//
+//  Created by Carlos Ramos on 10/04/14.
+//  Copyright (c) 2014 Carlos Ramos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CVCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ video-converter/CVCAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CVCAppDelegate.m
+//  video-converter
+//
+//  Created by Carlos Ramos on 10/04/14.
+//  Copyright (c) 2014 Carlos Ramos. All rights reserved.
+//
+
+#import "CVCAppDelegate.h"
+
+@implementation CVCAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
